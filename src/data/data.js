109b809a86cec26f43057dc8550bbac8424bf059
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-04-22 07:09:06 -0500

    added some mock data

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+export const mockDataTeam = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@email.com",
+      age: 35,
+      phone: "(410)444-4444",
+      access: "admin",
+    },
+    {
+      id: 2,
+      name: "Carlyle Jenkins",
+      email: "carlyle@email.com",
+      age: 42,
+      phone: "(444)444-4444",
+      access: "manager",
+    },
+    {
+      id: 3,
+      name: "Jimmy John",
+      email: "jimmy@email.com",
+      age: 45,
+      phone: "(222)222-2222",
+      access: "user",
+    },
+  ];
